db6e7707592af8cedbee46640d0aa374b26be281
ger <gerardroge75@gmail.com>
2020-01-27 12:39:09 +0100

    ajout doc technique

---
+++ src/image/Image.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package image;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author formation_gep
+ */
+public class Image {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    public static int [][]  mystere   
+    }
+    
+}
